d45c94167286812222805beb1d69ee77cc9280c5
Daniel Vartanov <dan@vartanov.net>
2014-03-15 17:16:00 +0000

    Add a magnet

--- graphics.rb
+++ graphics.rb
@@ -1,11 +1,38 @@
 module Gravity
   class Graphics < Struct.new(:screen, :world)
+    def initialize(*args)
+      super
+
+      extend_point_class
+    end
+
     def draw!
       draw_metal_ball
+      draw_magnets
     end
 
     def draw_metal_ball
-      screen.draw_circle_s(world.metal_ball.position, 3, [0x33, 0, 0xff])
+      screen.draw_circle_s(world.metal_ball.position.to_screen_coordinates, 5, [0, 191, 255])
+    end
+
+    def draw_magnets
+      world.magnets.each do |magnet|
+        screen.draw_circle_s(magnet.position.to_screen_coordinates, 3, [255, 90, 0])
+      end
+    end
+
+    def extend_point_class
+      screen_centre = screen.size.map { |axis| axis / 2 }
+
+      point_extension = Module.new do
+        define_method :to_screen_coordinates do
+          [screen_centre[0] + x, screen_centre[1] - y]
+        end
+      end
+
+      ::Geometry::Point.class_eval do
+        include point_extension
+      end
     end
   end
 end
--- logic.rb
+++ logic.rb
@@ -1,23 +1,60 @@
 module Gravity
+  include Geometry
+
   class World
     attr_reader :metal_ball
+    attr_reader :magnets
 
     def initialize
-      self.metal_ball = MetalBall.new ::Geometry::Point.new(0, 0), ::Geometry::Vector.new(30, 30)
+      self.metal_ball = MetalBall.new Geometry::Point.new(0, -400), Geometry::Vector.new(0, 90)
+      self.magnets = [
+                      Magnet.new(Geometry::Point.new(-50, 0), self)
+                     ]
     end
 
     def update!(seconds)
       metal_ball.update! seconds
+      magnets.each do |magnet|
+        magnet.update! seconds
+      end
     end
 
     protected
 
     attr_writer :metal_ball
+    attr_writer :magnets
   end
 
   class MetalBall < Struct.new(:position, :velocity)
     def update!(seconds)
       self.position = position.advance_by velocity * seconds
     end
+  end
+
+  class Magnet < Struct.new(:position, :world)
+    def update!(seconds)
+      metal_ball = world.metal_ball
+
+      distance = Geometry.distance(position, metal_ball.position)
+      attraction_force = 1000000 / distance ** 2
+
+      attraction_force_vector = Geometry::Vector.by_end_points(metal_ball.position, self.position).unit_vector * attraction_force * seconds
+
+      metal_ball.velocity = metal_ball.velocity + attraction_force_vector
+    end
+  end
+end
+
+Geometry::Vector.class_eval do
+  def self.by_end_points(origin_point, end_point)
+    new end_point.x - origin_point.x, end_point.y - origin_point.y
+  end
+
+  def /(scalar)
+    self * (1 / scalar.to_f)
+  end
+
+  def unit_vector
+    self / modulus
   end
 end
